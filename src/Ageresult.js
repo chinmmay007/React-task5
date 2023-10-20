69f39719a114bfaf263c5c8ab78c3c1cdccdf49a
Chinmmay <chinmmay007@gmail.com>
2023-10-20 10:05:00 +0530

    first commit

---
+++ src/Ageresult.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ageresult = ({ age }) => {
+  return (
+    <h4>
+      You are {age.years} years, {age.months} months, and {age.days} days old.
+    </h4>
+  );
+};
+
+export default Ageresult;
